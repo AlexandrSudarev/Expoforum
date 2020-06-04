511c412653ac6987ee5361db19e4f9c3848a8d60
AlexandrSudarev <7596056@gmail.com>
2020-06-04 14:36:22 +0300

    add search and javascript
    add search and javascript

---
+++ script/script.js
@@ -0,0 +1,25 @@
+let documentItems = document.querySelectorAll('body a, h2, p, h3, h1')
+document.querySelector('#input-search').oninput = function() {
+    let searchValue = this.value.trim();
+    if (searchValue != '') {
+        documentItems.forEach(function(elem) {
+            if (elem.innerText.search(searchValue) == -1) {
+                console.log('false')
+                elem.innerHTML = elem.innerText
+            } else {
+                let str = elem.innerText;
+                elem.innerHTML = insertMark(str, elem.innerText.search(searchValue), searchValue.length)
+                console.log('true')
+            }
+        })
+    } else {
+        documentItems.forEach(function(elem) {
+            elem.innerHTML = elem.innerText
+        });
+    }
+}
+
+function insertMark(string, position, len) {
+    //возвращает строку от начала до совпадения + <mark>Строку от точки совпадения до её длины</mark> + строка от точки совпадения до её длины
+    return string.slice(0, position) + '<mark>' + string.slice(position, position + len) + '</mark>' + string.slice(position + len);
+}
